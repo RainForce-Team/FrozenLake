7c12583f4d9c73fe1e8267e31ba2f74360289aa0
Gheeroppa <federico.ghir@gmail.com>
2018-05-23 18:28:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,154 +1,71 @@
-""" Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
-import numpy as np
-import pickle
 import gym
+import numpy as np
+import tensorflow as tf
 
-# hyperparameters ----------------------------------------------
-H = 200  # number of hidden layer neurons
-batch_size = 64  # every how many episodes to do a param update?
-learning_rate = 1e-4
-gamma = 0.90  # discount factor for reward
-decay_rate = 0.99  # decay factor for RMSProp leaky sum of grad^2
-resume = True  # resume from previous checkpoint?
-render = False
-################################################################
+env = gym.make('FrozenLake-v0')
 
-# model initialization ------------------------------------------------------------------------------------------------
-D = 80 * 80  # input dimensionality: 80x80 grid
-if resume:
-	#print("Model resumed")
-    model = pickle.load(open('save.p', 'rb'))
-else:
-    model = {}
-    model['W1'] = np.random.randn(H, D) / np.sqrt(D)  # "Xavier" initialization
-    model['W2'] = np.random.randn(H) / np.sqrt(H)
-
-grad_buffer = {k: np.zeros_like(v) for k, v in model.items()}  # update buffers that add up gradients over a batch
-rmsprop_cache = {k: np.zeros_like(v) for k, v in model.items()}  # rmsprop memory
+env.seed(1778)
+np.random.seed(1778)
+tf.set_random_seed(1778)
 
 
-#######################################################################################################################
+# The grid with 4x4 gives 16 possible states, hence we have an array of 16 states.
+inputs = tf.placeholder(shape=[1, 16], dtype=tf.float32)
+# Each state has 4 possible outcomes, hence we have 16x4 matrix with weights uniformly distributed
+weights = tf.Variable(tf.random_uniform([16, 4], 0, 0.1))
+# Find the dot product of inputs and the weights
+Q1 = tf.matmul(inputs, weights)
+# The next state will be the opted based on the argmax function.
+output = tf.argmax(Q1, 1)
 
-
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))  # sigmoid "squashing" function to interval [0,1]
-
-
-def prepro(I):
-    """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-    I = I[35:195]  # crop
-    I = I[::2, ::2, 0]  # downsample by factor of 2
-    I[I == 144] = 0  # erase background (background type 1)
-    I[I == 109] = 0  # erase background (background type 2)
-    I[I != 0] = 1  # everything else (paddles, ball) just set to 1
-    return I.astype(np.float).ravel()
-
-
-def discount_rewards(r):
-    """ take 1D float array of rewards and compute discounted reward """
-    discounted_r = np.zeros_like(r)
-    running_add = 0
-    for t in reversed(range(0, r.size)):
-        if r[t] != 0: running_add = 0  # reset the sum, since this was a game boundary (pong specific!)
-        running_add = running_add * gamma + r[t]
-        discounted_r[t] = running_add
-
-    return discounted_r
-
-
-def policy_forward(x):
-    h = np.dot(model['W1'], x)
-    h[h < 0] = 0  # ReLU nonlinearity
-    logp = np.dot(model['W2'], h)
-    p = sigmoid(logp)
-    return p, h  # return probability of taking action 2, and hidden state
-
-
-def policy_backward(eph, epdlogp, epx):
-    """ backward pass. (eph is array of intermediate hidden states) """
-    dW2 = np.dot(eph.T, epdlogp).ravel()
-    dh = np.outer(epdlogp, model['W2'])
-    dh[eph <= 0] = 0  # backpro prelu
-    dW1 = np.dot(dh.T, epx)
-    return {'W1':dW1, 'W2':dW2}
+Q2 = tf.placeholder(shape=[1, 4], dtype=tf.float32)
+loss = tf.reduce_sum(tf.square(Q2 - Q1))
+gdo = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+updatedweights = gdo.minimize(loss)
 
 
 def main():
-    env = gym.make("Pong-v0")
-    observation = env.reset()
-    prev_x = None  # used in computing the difference frame
-    xs, hs, dlogps, drs = [], [], [], []
-    running_reward = None
-    reward_sum = 0
-    episode_number = 0
+    GAMMA = 0.9
+    EPSILON = 0.1
+    EPISODES = 500
 
-    while True:
-        if render: env.render()
+    total_Reward = 0
 
-        # preprocess the observation, set input to network to be difference image
-        cur_x = prepro(observation)
-        x = cur_x - prev_x if prev_x is not None else np.zeros(D)
-        prev_x = cur_x
+    session = tf.Session()
+    session.run(tf.global_variables_initializer())
 
-        # forward the policy network and sample an action from the returned probability
-        aprob, h = policy_forward(x)
-        action = 2 if np.random.uniform() < aprob else 3  # roll the dice!
+    rList = []
 
-        # record various intermediates (needed later for backprop)
-        xs.append(x)  # observation
-        hs.append(h)  # hidden state
-        y = 1 if action == 2 else 0  # a "fake label"
-        dlogps.append(
-            y - aprob)  # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+    for i in range(EPISODES):
+        state_now = env.reset()
+        done = False
+        reward = 0
+        rAll = 0
+        for j in range(100):
+            # Lets find the dot product of the inputs with the weights and apply argmax on it.
+            action, Y = session.run([output, Q1], feed_dict={inputs: [np.eye(16)[state_now]]})
+            if EPSILON > np.random.rand(1):
+                action[0] = env.action_space.sample()
+                EPSILON -= 10 ** -3
+            # Lets iterate to the next state Note: This can be random.
+            state_next, reward, done, _ = env.step(action[0])
+            Y1 = session.run(Q1, feed_dict={inputs: [np.eye(16)[state_next]]})
+            change_Y = Y
+            change_Y[0, action[0]] = reward + GAMMA * np.max(Y1)
+            # Updating the weights
+            _, new_weights = session.run([updatedweights, weights],
+                                         feed_dict={inputs: [np.eye(16)[state_now]], Q2: change_Y})
+            # Lets append the total number of rewards
+            total_Reward += reward
+            rAll += reward
+            state_now = state_next
+            if reward == 1:
+                print ('Episode {} was successful, Agent reached the Goal'.format(i))
+        rList.append(rAll)
 
-        # step the environment and get new measurements
-        observation, reward, done, info = env.step(action)
-        reward_sum += reward
-
-        drs.append(reward)  # record reward (has to be done after we call step() to get reward for previous action)
-
-        if done:  # an episode finished
-            episode_number += 1
-
-            # stack together all inputs, hidden states, action gradients, and rewards for this episode
-            epx = np.vstack(xs)
-            eph = np.vstack(hs)
-            epdlogp = np.vstack(dlogps)
-            epr = np.vstack(drs)
-            xs, hs, dlogps, drs = [], [], [], []  # reset array memory
-
-            # compute the discounted reward backwards through time
-            discounted_epr = discount_rewards(epr)
-            # standardize the rewards to be unit normal (helps control the gradient estimator variance)
-            discounted_epr -= np.mean(discounted_epr)
-            discounted_epr /= np.std(discounted_epr)
-
-            epdlogp *= discounted_epr  # modulate the gradient with advantage (PG magic happens right here.)
-            grad = policy_backward(eph, epdlogp, epx)
-            for k in model: grad_buffer[k] += grad[k]  # accumulate grad over batch
-
-            # perform rmsprop parameter update every batch_size episodes
-            if episode_number % batch_size == 0:
-                for k, v in model.items():
-                    g = grad_buffer[k]  # gradient
-                    rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g ** 2
-                    model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
-                    grad_buffer[k] = np.zeros_like(v)  # reset batch gradient buffer
-
-            # boring book-keeping
-            running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-            print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
-            if episode_number % 100 == 0: pickle.dump(model, open('save.p', 'wb'))
-            reward_sum = 0
-            observation = env.reset()  # reset env
-            prev_x = None
-
-            if reward != 0:  # Pong has either +1 or -1 reward exactly when game ends.
-                #print ('ep %d: game finished, reward: %f' % (episode_number, reward)) + ('' if reward == -1 else ' !!!!!!!!')
-                print("ep " + str(episode_number) + ": game finished, reward: " + str(reward) + "%")
-                print('' if reward == -1 else ' !!!!!!!!')
+    session.close()
+    print("Score over time: " + str(sum(rList) / EPISODES))
 
 
-# DEBUG LOCAL #
 if __name__ == "__main__":
     main()
